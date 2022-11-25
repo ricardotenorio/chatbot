9b570d3603dce43a6239d0d020716caed684e4a2
ricardotenorio <ricardo.heber@gmail.com>
2022-11-25 02:25:40 -0300

    Modificação da função newsToCardResponse

--- server/src/utils/newsToCardResponse.js
+++ server/src/utils/newsToCardResponse.js
@@ -1,36 +1,45 @@
 module.exports = {
     newsToCardResponse(newsCollection) {
+        const fullfillment = { fulfillmentMessages: [] }
+
         if (newsCollection.length === 0) {
-            return {};
-        }
+            fullfillment.fulfillmentMessages.push({
+                text: {
+                    text: ['Nenhuma notícia encontrada.'],
+                },
+            });
+        } else {
 
-        const cards = newsCollection.map((news) => {
-            return {
-                title: news.title,
-                subtitle: news.description,
-                image_url: news.image_url,
-                buttons: [
-                    {
-                        url: news.news_url
-                    }
-                ]
-            }
-        })
+            const cards = newsCollection.map((news) => {
+                return {
+                    title: news.title,
+                    subtitle: news.description,
+                    image_url: news.image_url,
+                    buttons: [
+                        {
+                            postback: news.news_url,
+                        }
+                    ]
+                }
+            })
 
-        const cardsResponse = {
-            facebook: {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        elements: [
-                            cards
-                        ],
-                        template_type: "generic"
+            const cardsResponse = {
+                facebook: {
+                    attachment: {
+                        type: "template",
+                        payload: {
+                            elements: [
+                                cards
+                            ],
+                            template_type: "generic"
+                        }
                     }
                 }
             }
+
+            fullfillment.fulfillmentMessages.push(cardsResponse);
         }
 
-        return cardsResponse;
+        return fullfillment;
     }
 }
