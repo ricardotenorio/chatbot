9f21462cc5072aacf250fda7b57d1151778fddef
ricardotenorio <ricardo.heber@gmail.com>
2022-11-25 06:27:20 -0300

    testando resposta...(6)

--- server/src/utils/newsToCardResponse.js
+++ server/src/utils/newsToCardResponse.js
@@ -21,6 +21,7 @@ module.exports = {
                     {
                         type: 'web_url',
                         url: news.news_url,
+                        title: 'Visitar Site'
                     }
                 ]
             }
