450744d5f9271de9aa507b149dda89ffd58c5b9d
ricardotenorio <ricardo.heber@gmail.com>
2022-11-26 12:53:42 -0300

    Ajuste na tabela de notícias Visualização da imagem; Adicionado o elemento <a> para o link da notícia; Limite de caracteres exibidos no título e link.

--- webapp/src/components/Table.js
+++ webapp/src/components/Table.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+const IMAGE = {
+    maxWidth: '225px',
+    maxHeight: '118px',
+    width: 'auto',
+    height: 'auto',
+    borderRadius: '7px',
+}
+
 const Table = ({ data, loading, onSelect, openModal, action }) => {
 
     const handleClick = (event, news) => {
@@ -33,11 +41,18 @@ const Table = ({ data, loading, onSelect, openModal, action }) => {
                         data.map((news) => {
                             return (
                                 <tr key={news._id}>
-                                    <td>{news.image_url}</td>
-                                    <td>{news.title}</td>
+                                    <td><img src={news.image_url} style={IMAGE} /></td>
+                                    <td className='text-truncate' style={{ maxWidth: '30em' }}>{news.title}</td>
                                     <td className='text-truncate' style={{ maxWidth: '30em' }}>{news.description}</td>
                                     <td>{news.category}</td>
-                                    <td>{news.news_url}</td>
+                                    <td className='text-truncate' style={{ maxWidth: '30em' }}>
+                                        <a
+                                            href={news.news_url}
+                                            target='_blank'
+                                            className='link-dark'>
+                                            {news.news_url}
+                                        </a>
+                                    </td>
                                     <td><button className='btn btn-success' value='edit' onClick={(e) => handleClick(e, news)}>Editar</button></td>
                                     <td><button className='btn btn-danger' value='delete' onClick={(e) => handleClick(e, news)}>Deletar</button></td>
                                 </tr>
